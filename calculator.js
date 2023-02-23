05f190b010bfc60458515370f153106cb1f5c7f5
Wachirawit <grosserludwig@gmail.com>
2023-02-23 22:16:05 +0700

    linking JS / HTML buttons

--- calculator.js
+++ calculator.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let clear = document.querySelector(".clear");
+
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
+
+    let numbers = document.querySelectorAll(".number");
+    let operator = document.querySelectorAll(".operator");
+
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
+
+
+})
+
+
+
 /*
 const resultInput = document.getElementById('result');
 const buttons = document.querySelectorAll('button');
